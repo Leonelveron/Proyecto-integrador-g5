40bf6f0d349d682ecabd69d212985ffecaf240b8
Nicoglx <nicman_86@hotmail.com>
2020-05-28 23:28:12 -0300

    Corrección de footer

---
+++ controllers/carritoController.js
@@ -0,0 +1,7 @@
+const controlador = {
+    carrito: (req, res) => {
+            res.render('carrito');
+          }
+        };
+
+module.exports = controlador; 
---
+++ controllers/productosController.js
@@ -0,0 +1,7 @@
+const controlador = {
+    nuevoProducto: (req, res) => {
+            res.render('nuevoProducto')
+    }
+        };
+
+module.exports = controlador; 
