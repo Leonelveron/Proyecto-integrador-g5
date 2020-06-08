397116a12b99641f3faf0014712073feb7551ded
leonelveron <leonelveron.12@gmail.com>
2020-06-08 19:56:20 -0300

    se modifico controller de edit product

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -36,19 +36,22 @@ const controlador = {
     },
 
     indexEdit: (req, res) => {
-        let productDetail = products.filter(product => req.params.id == product.id)
-        res.render('productEdit', { productDetail: productDetail })
+        let productDetail = products.filter(product => req.params.id == product.id);
+        res.render('productEdit', { productDetail: productDetail });
     },
 
     editProduct: (req, res) => {
-        let productToModify = products.filter(product => req.params.id == product.id)
+        let productEdited = req.body
+        let productEditedJSON = JSON.stringify(productEdited);
+        fs.writeFileSync('./data/products.json', productEditedJSON)
+        res.render('productEdit', { productEdited: productEdited })
     },
 
     deleteProduct: (req, res) => {
         let productDeleted = products.filter(product => product.id != req.params.id)
         let productDeletedJSON = JSON.stringify(productDeleted)
         fs.writeFileSync('./data/products.json', productDeletedJSON)
-        res.redirect('/')
+        res.redirect('/products/detail')
     }
 };
 
