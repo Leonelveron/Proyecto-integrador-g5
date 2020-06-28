181b37f520fbf7f5f5c3a8b0c24d3dc2e44b2a7b
Nicoglx <nicman_86@hotmail.com>
2020-06-28 12:20:45 -0300

    Modifico controlador para vista de creacion de producto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -9,82 +9,85 @@ const controlador = {
     list: (req, res) => {
 
         db.Products.findAll({
-            include: [{association : "brands"}]
+            include: [{ association: "brands" }]
         })
-        .then(function (products) {
-            res.render("productsList2",{products: products});
-           })
+            .then(function (products) {
+                res.render("productsList2", { products: products });
+            })
 
         /* let allProducts = products.filter(product => product.stock > 0)
         res.render('productsList2', { allProducts }) */
     },
 
     index: (req, res) => {
-        res.render('nuevoProducto')
+        db.Brands.findAll({})
+            .then(function (brand) {
+                res.render('nuevoProducto', { brand: brand })
+            })
     },
 
     create: (req, res) => {
-        db.Brands.findAll().then(function(brand){
-            
-            res.render('nuevoProducto', {"brand": brand}) 
-        
-        }) 
-        
+        db.Brands.findAll().then(function (brand) {
+
+            res.render('nuevoProducto', { "brand": brand })
+
+        })
+
     },
 
     detail: (req, res) => {
 
         db.Products.findByPk(req.params.id,
-        ).then(function (product){
-            res.render("producto", { product: product})
+        ).then(function (product) {
+            res.render("producto", { product: product })
         })
 
-/* 
-        let productDetail = products.filter(product => req.params.id == product.id)
-        res.render('producto', { productDetail: productDetail }) */
+        /* 
+                let productDetail = products.filter(product => req.params.id == product.id)
+                res.render('producto', { productDetail: productDetail }) */
     },
 
 
-   /* Con DB */
-   
-   created : function (req, res) {
-            db.Product.create({
-                name: req.body.name ,
-                id_brand: req.body.id_brand ,
-                price: req.body.price,
-                description: req.body.description,
-            }).then(function (product){
-             
-        
-                res.redirect("/products/detail/req.params.id");
-               })
-        },
+    /* Con DB */
 
- 
- 
-/* Antes DB  */
-  /*       let jsonproduct = fs.readFileSync("../data/product.json", { encoding: "utf-8" })
-        let productos = []
-        let producto = {
-            nombre: req.body.nombre,
-            stock: req.body.stock,
-            precio: req.body.precio,
-        }
+    created: function (req, res) {
+        db.Product.create({
+            name: req.body.name,
+            id_brand: req.body.id_brand,
+            price: req.body.price,
+            description: req.body.description,
+        }).then(function (product) {
 
-        productos = JSON.parse(jsonproduct)
 
-        productos.push(producto)
-        let productosJSON = JSON.stringify(productos) 
- */
+            res.redirect("/products/detail/req.params.id");
+        })
+    },
+
+
+
+    /* Antes DB  */
+    /*       let jsonproduct = fs.readFileSync("../data/product.json", { encoding: "utf-8" })
+          let productos = []
+          let producto = {
+              nombre: req.body.nombre,
+              stock: req.body.stock,
+              precio: req.body.precio,
+          }
+  
+          productos = JSON.parse(jsonproduct)
+  
+          productos.push(producto)
+          let productosJSON = JSON.stringify(productos) 
+   */
 
     indexEdit: (req, res) => {
         let pedidoProducto = db.Products.findByPk(req.params.id);
         let pedidoMArcas = db.Brands.findAll();
         Promise.all([pedidoProducto, pedidoMArcas])
-        .then(function([product, brand]){
-            res.render('productEdit', { product: product, brand: brand })
-        })
-        
+            .then(function ([product, brand]) {
+                res.render('productEdit', { product: product, brand: brand })
+            })
+
     },
 
     editProduct: (req, res) => {
