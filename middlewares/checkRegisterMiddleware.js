56e5573d9153bdf8861d1beadeceb168fbb2eb8e
Nicoglx <nicman_86@hotmail.com>
2020-07-03 19:45:41 -0300

    Agrego modulos

--- middlewares/checkRegisterMiddleware.js
+++ middlewares/checkRegisterMiddleware.js
@@ -7,15 +7,17 @@ let registerMiddleware = [
     check('email').isEmail().withMessage('El campo "Email" debe tener un mail válido'),
     check('password').isLength({ min: 6 }).withMessage("La contraseña debe tener por lo menos 6 caracteres"),
     body('email').custom(function (value) {
-        db.Users.findAll()
-            .then(function (users) {
-                for (let i = 0; i < users.length; i++) {
-                    if (users[i].mail == value) {
-                        return false
-                    }
-                }
-                return true
-            })
+        db.Users.findOne({
+            where: {
+                mail: value
+            }
+        }).then(function (user) {
+            console.log(user)
+            if (user != null) {
+                Promise.reject()
+            }
+            Promise.resolve()
+        })
     }).withMessage("El mail ya existe"),
 ]
 
