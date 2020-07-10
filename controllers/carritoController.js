96c2f2f8c56518a408160653f51fff8e23c96529
Nicoglx <nicman_86@hotmail.com>
2020-07-10 19:58:56 -0300

    Modifico estilos del carrito y agrego cambio de precio dinamico

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,7 +1,18 @@
+const db = require("../db/models");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+
 const controlador = {
-    carrito: (req, res) => {
-            res.render('carrito');
-          }
-        };
+  carrito: (req, res) => {
+    db.Products.findAll({
+      include: [{ association: "brands" }],
+      limit: 3
+    })
+      .then((products) => {
+        res.render('carrito', { products: products });
+      });
+  }
+};
 
 module.exports = controlador; 
