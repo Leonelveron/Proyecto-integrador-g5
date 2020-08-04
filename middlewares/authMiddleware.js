33e0678c8ca275a4633198704fa5fe38232168f2
Nicoglx <nicman_86@hotmail.com>
2020-08-03 21:08:44 -0300

    Agrego vista para MW de visitante

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ function authMiddleware(req, res, next) {
         next()
     }
     else {
-        res.send('Esta página es solo para usuarios logueados')
+        res.render('cartelVisitante')
     }
 }
 
